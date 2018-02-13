303759810ffc9c8f316f0471816592d21a2d8506
Peter Somers <peter.somers@sony.com>
2018-02-13 15:46:15 +0100

    wip

--- src/main/java/pso/decision_engine/persistence/impl/RuleSetDaoImpl.java
+++ src/main/java/pso/decision_engine/persistence/impl/RuleSetDaoImpl.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import pso.decision_engine.model.RuleSet;
+
 @Component
 public class RuleSetDaoImpl {
 	
@@ -20,33 +22,45 @@ public class RuleSetDaoImpl {
     private static String createTables[]=
 	{
 		"CREATE TABLE IF NOT EXISTS RuleSet ("+
-		"ruleSetId varchar(20) NOT NULL, "+
-		"restEndPoint varchar(20) NOT NULL, "+
-		"createdBy varchar(50), "+
-		"version varchar(20), "+
+		"ruleSetId VARCHAR(20) NOT NULL, "+
+		"restEndPoint VARCHAR(20) NOT NULL, "+
+		"createdBy VARCHAR(50), "+
+		"version VARCHAR(20), "+
 		"uploadDate TIMESTAMP NOT NULL, "+
 		"PRIMARY KEY (ruleSetId))",
 
 		"CREATE TABLE IF NOT EXISTS RuleSetParameters ("+
-		"ruleSetId varchar(20) NOT NULL, "+
-		"parameterName varchar(40) NOT NULL, "+
-		"parameterType varchar(10) NOT NULL, "+
+		"ruleSetId VARCHAR(20) NOT NULL, "+
+		"parameterName VARCHAR(40) NOT NULL, "+
+		"parameterType VARCHAR(10) NOT NULL, "+
 		"PRIMARY KEY (ruleSetId, parameterName))",
 		
 		"CREATE TABLE IF NOT EXISTS Rule ("+
-		"ruleSetId varchar(20) NOT NULL, "+
+		"ruleSetId VARCHAR(20) NOT NULL, "+
 		"ruleNumber INTEGER NOT NULL, "+
 		"sheetName VARCHAR(100), "+
 		"rowNumber INTEGER NOT NULL, "+
 		"rowLabel VARCHAR(100), "+
-		"parameterName varchar(40), "+
-		"comparator varchar(15), "+
-		"value1 varchar(100), "+
-		"value2 varchar(100), "+
-		"positiveResult varchar(200), "+
-		"negativeResult varchar(200), "+
-		"remark varchar(500), "+
-		"PRIMARY KEY (ruleSetId, ruleNumber))"
+		"parameterName VARCHAR(40), "+
+		"comparator VARCHAR(15), "+
+		"value1 VARCHAR(100), "+
+		"value2 VARCHAR(100), "+
+		"positiveResult VARCHAR(200), "+
+		"negativeResult VARCHAR(200), "+
+		"remark VARCHAR(500), "+
+		"PRIMARY KEY (ruleSetId, ruleNumber))",
+		
+		"CREATE TABLE IF NOT EXISTS RuleSetList ("+
+		"ruleSetId VARCHAR(20) NOT NULL, "+
+		"listId INTEGER NOT NULL, "+
+		"listName VARCHAR(100), "+
+		"PRIMARY KEY (ruleSetId, listId))",
+		
+		"CREATE TABLE IF NOT EXISTS RuleSetListValues ("+
+		"ruleSetId VARCHAR(20) NOT NULL, "+
+		"listId INTEGER NOT NULL, "+
+		"listValue VARCHAR(100) NOT NULL, "+
+		"PRIMARY KEY (ruleSetId, listId, listValue))"
 	};
     
 	@PostConstruct
@@ -56,7 +70,10 @@ public class RuleSetDaoImpl {
 		for (String sql:createTables) {
 			jdbcTemplate.update(sql, params);
 		}
-			
+	}
+	
+	public void saveRuleSet(RuleSet ruleSet) {
+		
 	}
 
 }
