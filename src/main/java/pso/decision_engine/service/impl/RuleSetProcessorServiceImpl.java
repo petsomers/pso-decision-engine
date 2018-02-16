c89f2b97c17e09b07955acc3d6728257d822501e
Peter Somers <peter.somers@sony.com>
2018-02-16 16:55:47 +0100

    wip

--- src/main/java/pso/decision_engine/service/impl/RuleSetProcessorServiceImpl.java
+++ src/main/java/pso/decision_engine/service/impl/RuleSetProcessorServiceImpl.java
@@ -1,11 +1,13 @@
 package pso.decision_engine.service.impl;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 import pso.decision_engine.model.DecisionResult;
 import pso.decision_engine.model.DecisionTrace;
+import pso.decision_engine.model.DecisionTraceElement;
 import pso.decision_engine.model.Rule;
 import pso.decision_engine.model.RuleSet;
 import pso.decision_engine.model.enums.Comparator;
@@ -25,17 +27,43 @@ public class RuleSetProcessorServiceImpl {
 		trace.setRestEndPoint(rs.getRestEndPoint());
 
 		final HashMap<String, Object> typedParameters=toTypedParameters(rs, parameters, trace);
-		if (typedParameters==null) return result;
+		if (typedParameters==null) {
+			result.setError(true);
+			return result;
+		}
 		
 		Integer ruleNumber=rs.getRowLabels().get("START");
 		if (ruleNumber==null) ruleNumber=0; 
 		final int ruleCount=rs.getRules().size();
 		while (ruleNumber<ruleCount) {
 			Rule r=rs.getRules().get(ruleNumber);
-			String ruleResult=evaluateRule(rs, r, typedParameters);
-
+			boolean ruleResult=evaluateRule(rs, r, typedParameters);
+			DecisionTraceElement dte=new DecisionTraceElement();
+			trace.getTrace().add(dte);
+			dte.setRule(r);
+			dte.setResult(ruleResult);
+			String action=ruleResult?r.getPositiveResult():r.getNegativeResult();
+			if (action==null || action.isEmpty()) {
+				ruleNumber++;
+				continue;
+			}
+			if (action.startsWith("goto ") && action.length()>5) {
+				String label=action.substring(5);
+				Integer toRuleNumber=rs.getRowLabels().get("START");
+				if (toRuleNumber==null) {
+					trace.getMessages().add("ERROR: Label not found: "+label);
+					result.setError(true);
+					return result;
+				}
+				ruleNumber=toRuleNumber;
+				continue;
+			}
+			result.setDecision(action);
+			break;
 		}
 		trace.setResponseTimestamp(LocalDateTime.now());
+		trace.setDurationInMilliSeconds(trace.getResponseTimestamp().toEpochSecond(ZoneOffset.UTC) 
+				- trace.getRequestTimestamp().toEpochSecond(ZoneOffset.UTC));
 		return result;
 	}
 	
@@ -79,7 +107,7 @@ public class RuleSetProcessorServiceImpl {
 		return typedParameters;
 	}
 	
-	private String evaluateRule(RuleSet rs, Rule r, HashMap<String, Object> parameters) {
+	private boolean evaluateRule(RuleSet rs, Rule r, HashMap<String, Object> parameters) {
 		ParameterType type=rs.getInputParameters().get(r.getParameterName());
 		Object parameterValue=parameters.get(r.getParameterName());
 		boolean evalResult=false;
@@ -88,7 +116,7 @@ public class RuleSetProcessorServiceImpl {
 			case INTEGER: evalResult=compare((Integer)parameterValue, r.getComparator(), r.getValue1(), r.getValue2());break;
 			case DECIMAL: evalResult=compare((Double)parameterValue, r.getComparator(), r.getValue1(), r.getValue2());break;
 		}
-		return evalResult?r.getPositiveResult():r.getNegativeResult();
+		return evalResult;
 	}
 	
 	private boolean compare(String parameterValue, Comparator comparator, String value1, String value2) {
