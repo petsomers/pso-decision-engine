46f2cebf13d02674fdd5cea07d1488e6d6305df4
Peter Somers <peter.somers@sony.com>
2018-02-20 22:40:31 +0100

    wip

--- src/main/js/components/RuleSetSelection.js
+++ src/main/js/components/RuleSetSelection.js
@@ -1,19 +1,36 @@
 import React from "react";
 
 export const RuleSetSelection = ({restEndPoints, versions, layout}) => {
-	const restEndPointsHeight=layout.windowHeight-65/2;
+	const restEndPointsHeight=(layout.windowHeight-65)/2;
+	const versionsHeight=layout.windowHeight-restEndPointsHeight-90;
 	const uploadStyle={position:"fixed", top:"60px", left:"0px"};
-	const restEndPointsStyle={position:"fixed", top:"80px", height:restEndPointsHeight+"px", left:"0px", width: "300px", backgroundColor: "#CCCCCC"};
-    return (
+	const restEndPointsStyle={
+		position:"fixed",
+		top:"80px",
+		height:restEndPointsHeight+"px",
+		left:"0px",
+		width: "300px",
+		backgroundColor: "#CCCCCC"
+	};
+	const versionsStyle={
+		position:"fixed",
+		top:(85+restEndPointsHeight)+"px",
+		height:versionsHeight+"px",
+		left:"0px",
+		width: "300px",
+		backgroundColor: "#CCCCCC"
+	};
+  return (
     		<div>
 					<div style={uploadStyle}>
 					Upload Excel
 					</div>
 					<div style={restEndPointsStyle}>
-					endpoints
+					Rest Endpoints
 					</div>
-					Rest Endpoints<br /><br />
-					Versions
+					<div style={versionsStyle}>
+					Rest Endpoints
+					</div>
     		</div>
    );
 }
