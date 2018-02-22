8baaf30586e71bcc55b757ac25506a3d8d0ba669
Peter Somers <peter.somers@sony.com>
2018-02-22 09:22:12 +0100

    wip

--- src/main/js/components/RuleSetSelection.js
+++ src/main/js/components/RuleSetSelection.js
@@ -2,40 +2,56 @@ import React from "react";
 import { Button } from "react-lightning-design-system";
 
 export const RuleSetSelection = ({restEndpoints, versions, layout, openFileUpload, selectEndpoint}) => {
-	const restEndpointsHeight=(layout.windowHeight-60)/4;
+	const restEndpointsHeight=(layout.windowHeight-120)/3-5;
+	const versionsHeight=restEndpointsHeight;
+	const globalListsHeight=restEndpointsHeight;
 
-	const versionsHeight=restEndpointsHeight*2;
-
-	const globalListStyle={
-		position:"fixed", top:(restEndpointsHeight+versionsHeight+70)+"px", left:"0px",
-		overflowX: "hidden",overflowY: "scroll"
-	};
+	const restEndpointTitlePosition=60;
+	const versionTitlePosition=87+restEndpointsHeight;
+	const globalListTitlePosition=27+versionTitlePosition+versionsHeight;
 
 	const uploadStyle={
 		position:"fixed", top:(layout.windowHeight-50)+"px", left:"0px"
 	};
 
+	const restEndpointsTitleStyle={
+		position:"fixed", top:(restEndpointTitlePosition)+"px",
+	}
+	const versionTitleStyle={
+		position:"fixed", top:(versionTitlePosition)+"px",
+	}
+	const globalListTitleStyle={
+		position:"fixed", top:(globalListTitlePosition)+"px",
+	}
 	const restEndpointsStyle={
-		position:"fixed", top:"60px",
+		position:"fixed", top:(restEndpointTitlePosition+20)+"px",
 		height:restEndpointsHeight+"px",
 		left:"5px",	width: "300px",
 		overflowX: "hidden",overflowY: "scroll"
 	};
 	const versionsStyle={
 		position:"fixed",
-		top:(65+restEndpointsHeight)+"px",
+		top:(versionTitlePosition+20)+"px",
 		height:versionsHeight+"px",
 		left:"5px",	width: "300px",
 		overflowX: "hidden",overflowY: "scroll"
 	};
+	const globalListStyle={
+		position:"fixed",
+		top:(globalListTitlePosition+20)+"px",
+		height:globalListsHeight+"px",
+		left:"5px",	width: "300px",
+		overflowX: "hidden",overflowY: "scroll"
+	};
+
 	const cardStyle= {
 		padding: "5px",
 		whiteSpace: "nowrap"
 	}
   return (
   		<div>
+				<div style={restEndpointsTitleStyle}><b>Rest Endpoints</b></div>
 				<div style={restEndpointsStyle} className='slds-table slds-table--bordered'>
-					<b>Rest Endpoints</b>
 					{restEndpoints.map((endpoint, index) => (
 						<div key={endpoint} style={cardStyle} className='slds-table slds-table--bordered'>
 							<a onClick={() => selectEndpoint(endpoint)}>
@@ -44,8 +60,8 @@ export const RuleSetSelection = ({restEndpoints, versions, layout, openFileUploa
 						</div>
 					))}
 				</div>
+				<div style={versionTitleStyle}><b>Versions</b></div>
 				<div style={versionsStyle}>
-					<b>Versions</b>
 					{versions.map((version, index) => (
 						<div key={version.id} className='slds-table slds-table--bordered' style={cardStyle}>
 							Uploaded: <a onClick={() => selectVersion(version.id)}>{version.uploadDate}</a>
@@ -57,8 +73,9 @@ export const RuleSetSelection = ({restEndpoints, versions, layout, openFileUploa
 						</div>
 					))}
 				</div>
+				<div style={globalListTitleStyle}><b>Global Lists</b></div>
 				<div style={globalListStyle}>
-					Global Lists (all endpoints and versions)
+					Global Lists (for all endpoints and versions)
 				</div>
 				<div style={uploadStyle}>
 					<Button type="neutral" onClick={() => openFileUpload()} icon="new" iconAlign="left" label="Upload Excel" />
