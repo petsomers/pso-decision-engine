f957d4bbeea53dd88e6e4c93771b62b29b3bf992
Peter Somers <peter.somers@sony.com>
2018-02-22 15:58:03 +0100

    wip

--- src/main/js/components/rulesetdetails/Lists.js
+++ src/main/js/components/rulesetdetails/Lists.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { List } from 'react-virtualized';
 
 export const Lists = ({lists}) => {
 
   return (
     <div>
-      Lists ...
+      {Object.keys(lists).map((listName, index1) => (
+        <div>
+          <i class="fas fa-list"></i> <b>{listName}</b> ({lists[listName].length})
+          <div style={{paddingLeft: "20px"}}>
+            {lists[listName].length<10 &&
+              <div style={{width:"400px"}}>
+                {lists[listName].map((item, index2) => (
+                  <div>
+                  {item}<br />
+                  </div>
+                ))}
+              </div>
+            }
+            {lists[listName].length>=10 &&
+              <div style={{height:"200px", width:"400px", overflowY:"scroll"}}>
+              {lists[listName].map((item, index2) => (
+                <div>
+                {item}<br />
+                </div>
+              ))}
+              </div>
+            }
+          </div>
+          <br /><br />
+        </div>
+      ))}
     </div>
   );
 };
