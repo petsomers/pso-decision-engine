8086e3cbab3af2ce956b12d2887996ecffd27553
Peter Somers <peter.somers@sony.com>
2018-02-22 15:09:18 +0100

    wip

--- src/main/js/components/RuleSetDetails.js
+++ src/main/js/components/RuleSetDetails.js
@@ -20,7 +20,24 @@ export class  RuleSetDetails extends React.Component {
 	render() {
 		let d=this.props.ruleSetDetails;
 		let activeTab=this.state.activeTab;
+
 		const tableRowStyle={borderBottom: 'solid',borderBottomWidth: '1px',borderBottomColor:'#CCCCCC'}
+		const sheetNumberStyle={display: "inline-block", width: "30px", verticalAlign: "text-top", textAlign: "right"}
+		const ruleItemStyle={display: "inline-block", verticalAlign: "text-top", paddingLeft:"5px"}
+
+		let sheets=[];
+		if (activeTab=="rules") {
+			let currentRuleSheet="XXXXXXXXXXXXXXXXXXXXX";
+			let currentRuleSheetRuleList=[];
+			d.rules.map((rule, index) => {
+				if (currentRuleSheet!=rule.sheetName) {
+					currentRuleSheet=rule.sheetName;
+					currentRuleSheetRuleList=[];
+					sheets.push({sheetName: rule.sheetName, rules:currentRuleSheetRuleList});
+				}
+				currentRuleSheetRuleList.push(rule);
+			});
+		}
 		return (
 		<div>
 			<div style={{display: "inline-block", width: "350px"}}>
@@ -45,6 +62,8 @@ export class  RuleSetDetails extends React.Component {
 				<Tab eventKey="rules" title="Rules" />
 				<Tab eventKey="lists" title="Lists" />
 				<Tab eventKey="unitTests" title="Unit Tests" />
+				<Tab eventKey="runNow" title="Run" />
+				<Tab eventKey="statistics" title="Usage Statistics" />
 			</Tabs>
 			<div style={{backgroundColor: "white"}}>
 				{(activeTab=="inputParameters") &&
@@ -64,6 +83,52 @@ export class  RuleSetDetails extends React.Component {
 						</tr>
 					))}
 					</table>
+				}
+				{(activeTab=="rules") &&
+					<div>
+						{sheets.map((sheet, index1) => (
+							<div key={"sheet"+index1}>
+								<p><i class="far fa-file-excel"></i> &nbsp;&nbsp; <b>Excel Sheet:</b> {sheet.sheetName}</p>
+								{sheet.rules.map((rule, index2) => (
+									<div>
+										<div style={sheetNumberStyle}>
+											{rule.rowNumber}
+										</div>
+										<div style={ruleItemStyle}>
+											{rule.label!="" &&
+												<div>
+												<i class="fas fa-tags"></i> &nbsp; {rule.label}
+												</div>
+											}
+											<i class="fas fa-balance-scale"></i> &nbsp;&nbsp;<i>{rule.parameterName}</i> <b>{rule.comparator}</b> {rule.value1}
+											{rule.value2!="" &&
+												<span> ; {rule.value2}</span>
+											}
+											<br />
+											<i class="fas fa-check"></i> &nbsp;&nbsp;&nbsp;
+											{rule.positiveResult.startsWith("goto ") &&
+												<i class="fas fa-forward"></i>
+											}
+											{rule.positiveResult}<br />
+
+											<i class="fas fa-times"></i> &nbsp;&nbsp;&nbsp;
+											{rule.negativeResult.startsWith("goto ") &&
+												<i class="fas fa-forward"></i>
+											}
+											{rule.negativeResult}<br />
+
+											{rule.remark!="" &&
+												<span>
+													<i class="fas fa-info"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {rule.remark}<br />
+												</span>
+											}
+											<br />
+										</div>
+									</div>
+								))}
+								</div>
+							))}
+					</div>
 				}
 			</div>
 		</div>
--- src/main/resources/templates/app.ftl
+++ src/main/resources/templates/app.ftl
@@ -6,7 +6,7 @@
 <title>Decision Engine</title>
 <script src="//cdn.polyfill.io/v2/polyfill.min.js"></script>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/design-system/2.4.3/styles/salesforce-lightning-design-system.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
 </head>
 <body>
