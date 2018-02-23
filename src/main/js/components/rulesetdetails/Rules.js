1dfd6f6722f7d2ada5316a86b92266e28a8c2fc5
Peter Somers <peter.somers@sony.com>
2018-02-23 19:18:42 +0100

    wip

--- src/main/js/components/rulesetdetails/Rules.js
+++ src/main/js/components/rulesetdetails/Rules.js
@@ -3,7 +3,10 @@ import React from "react";
 export const Rules = ({rules}) => {
   const sheetNumberStyle={display: "inline-block", width: "30px", verticalAlign: "text-top", textAlign: "right"}
   const ruleItemStyle={display: "inline-block", verticalAlign: "text-top", paddingLeft:"5px"}
-
+  const conditionStyle={width:"660px", border:"solid", borderWidth:"1px", borderColor:"#CCCCCC", paddingLeft: "5px", borderBottom:"none"}
+  const remarkStyle={width:"660px", border:"solid", borderWidth:"1px", borderColor:"#CCCCCC", paddingLeft: "5px", borderTop:"none"}
+  const resultStyleGreen={display: "inline-block", color: "green", width:"330px", border:"solid", borderWidth:"1px", borderColor:"#CCCCCC", paddingLeft: "5px"}
+  const resultStyleRed={display: "inline-block", color: "red", width:"330px", border:"solid", borderWidth:"1px", borderColor:"#CCCCCC", paddingLeft: "5px"}
   let sheets=[];
   let currentRuleSheet="XXXXXXXXXXXXXXXXXXXXX";
   let currentRuleSheetRuleList=[];
@@ -21,45 +24,41 @@ export const Rules = ({rules}) => {
         <div key={"sheet"+index1}>
           <p><i className="far fa-file-excel"></i> &nbsp;&nbsp; <b>Excel Sheet:</b> {sheet.sheetName}</p>
           {sheet.rules.map((rule, index2) => (
-            <div>
+            <div style={{paddingBottom: "20px"}}>
               <div style={sheetNumberStyle}>
                 {rule.rowNumber}
               </div>
               <div style={ruleItemStyle}>
                 {rule.label!="" &&
-                  <div>
+                  <div style={conditionStyle}>
                   <i className="fas fa-tags"></i> &nbsp; {rule.label}
                   </div>
                 }
-                <i className="fas fa-balance-scale"></i> &nbsp;&nbsp;<i>{rule.parameterName}</i> <b>{rule.comparator}</b> {rule.value1}
-                {rule.value2!="" &&
-                  <span> ; {rule.value2}</span>
-                }
-                <br />
-                <i className="fas fa-check"></i> &nbsp;&nbsp;&nbsp;
-                {rule.positiveResult.startsWith("goto ") &&
-                  <span><i className="fas fa-forward"></i>&nbsp;&nbsp;</span>
-                }
-                {rule.positiveResult=="" &&
-                  <i class="fas fa-level-down-alt"></i>
-                }
-                {rule.positiveResult}<br />
-
-                <i className="fas fa-times"></i> &nbsp;&nbsp;&nbsp;&nbsp;
-                {rule.negativeResult.startsWith("goto ") &&
-                  <span><i className="fas fa-forward"></i>&nbsp;&nbsp;</span>
-                }
-                {rule.negativeResult=="" &&
-                  <i class="fas fa-level-down-alt"></i>
-                }
-                {rule.negativeResult}<br />
-
-                {rule.remark!="" &&
-                  <span>
-                    <i className="fas fa-info"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {rule.remark}<br />
-                  </span>
-                }
-                <br />
+                <div style={conditionStyle}>
+                  <i className="fas fa-balance-scale"></i> &nbsp;&nbsp;<i>{rule.parameterName}</i> <b>{rule.comparator}</b> {rule.value1}
+                  {rule.value2!="" &&
+                    <span> ; {rule.value2}</span>
+                  }
+                </div>
+                <div style={resultStyleGreen}>
+                  <i className="fas fa-check"></i> &nbsp;&nbsp;&nbsp;
+                  {rule.positiveResult=="" &&
+                    <i class="fas fa-level-down-alt"></i>
+                  }
+                  {rule.positiveResult}
+                </div>
+                <div style={resultStyleRed}>
+                  <i className="fas fa-times"></i> &nbsp;&nbsp;&nbsp;&nbsp;
+                  {rule.negativeResult=="" &&
+                    <i class="fas fa-level-down-alt"></i>
+                  }
+                  {rule.negativeResult}<br />
+                  </div>
+                  {rule.remark!="" &&
+                    <div style={remarkStyle}>
+                      <i className="fas fa-info"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {rule.remark}<br />
+                    </div>
+                  }
               </div>
             </div>
           ))}
