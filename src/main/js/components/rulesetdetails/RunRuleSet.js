e48d9305fc3cc195643e2a68bc5cfc4cc2822d34
Peter Somers <peter.somers@sony.com>
2018-02-25 12:41:22 +0100

    wip

--- src/main/js/components/rulesetdetails/RunRuleSet.js
+++ src/main/js/components/rulesetdetails/RunRuleSet.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Input } from "react-lightning-design-system";
+import { Input, Button } from "react-lightning-design-system";
 
 export const RunRuleSet = ({inputParameters, runNowData, setRunNowParameterValue}) => {
     return (
-      <div>
-        {Object.keys(inputParameters).map((parameterName, index) => (
-            <Input
-              key={index}
-              value={runNowData.parameterValues[parameterName]}
-              label={parameterName}
-              onChange={(event) => setRunNowParameterValue(parameterName, event.target.value)}/>
-        ))}
-      </div>
+<div>
+  <div style={{position: "absolute", right: "left: 280px", top: "135px"}}>
+		<Button type="brand" onClick={() => run()} icon="right" iconAlign="left" label="Run" />
+	</div>
+  <div style={{display: "inline-block", width: 200}}>
+    {Object.keys(inputParameters).map((parameterName, index) => (
+      <Input
+        key={index}
+        value={runNowData.parameterValues[parameterName]}
+        label={parameterName}
+        onChange={(event) => setRunNowParameterValue(parameterName, event.target.value)}/>
+      ))}
+  </div>
+  <div style={{display: "inline-block"}}>
+    RUN RESULT
+  </div>
+</div>
     )
 };
