c61a4b705e1a106ddc290321d4267057bc47d7c3
Peter Somers <peter.somers@sony.com>
2018-03-02 10:17:10 +0100

    wip

---
+++ src/main/js/actions/appActions.js
@@ -0,0 +1,124 @@
+import axios from "axios"
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onWindowResize: (width, height) => {
+			dispatch({
+				type: "WINDOW_RESIZE",
+				payload: {width, height}
+			});
+		},
+		doClearMessages() {
+			dispatch({type: "CLEAR_MESSAGES"});
+		},
+		openFileUpload: (width, height) => {
+			dispatch({
+				type: "GOTO_UPLOAD",
+				payload: {width, height}
+			});
+		},
+		setSelectedVersion: (endpoint, version) => {
+			dispatch({
+				type: "SET_SELECTED_VERSION",
+				payload: {endpoint,version}
+			});
+		},
+		loadEndpoints: () => {
+			console.log("LOAD ENDPOINTS.....");
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+  			type: "GET_ENDPOINTS",
+  			payload: axios.get("setup/endpoints")
+			});
+		},
+		loadDataSets: () => {
+			console.log("LOAD LISTS.....");
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+  			type: "GET_DATASETS",
+  			payload: axios.get("dataset/all")
+			});
+		},
+		selectEndpoint: (endpoint) => {
+			console.log("LOAD VERSIONS for "+endpoint);
+			dispatch({
+				type: "SET_SELECTED_ENDPOINT",
+				payload: endpoint
+			});
+			//dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+  			type: "GET_VERSIONS",
+  			payload: axios.get("setup/versions/"+endpoint)
+			});
+		},
+		selectVersion: (endpoint, versionId) => {
+			console.log("LOAD RULESET for "+endpoint+"/"+versionId);
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+  			type: "GET_RULESET",
+  			payload: axios.get("setup/source/"+endpoint+"/"+versionId)
+			});
+		},
+		runUnitTests: (endpoint, versionId) => {
+			console.log("RUNNING UNIT TESTS for "+endpoint+"/"+versionId);
+			//dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+  			type: "RUN_UNIT_TESTS",
+  			payload: axios.get("run_unittests/"+endpoint+"/"+versionId)
+			});
+		},
+		setRunNowParameterValue: (name, value) => {
+			dispatch({
+  			type: "SET_RUNNOW_PARAMETER_VALUE",
+  			payload: {parameterName: name, parameterValue: value}
+			});
+		},
+		runNow: (endpoint, versionId, parameters) => {
+			var parameterStr="";
+			Object.keys(parameters).map((parameterName, index) => {
+				parameterStr+= "&"+parameterName+'='+encodeURIComponent(parameters[parameterName]);
+			});
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+				type: "RUN_NOW",
+				payload: axios.get("json/run/"+endpoint+"/"+versionId+"?trace=Y&"+parameterStr)
+			});
+		},
+		runNowClearResult: () => {
+			dispatch({type: "RUN_NOW_CLEAR_RESULT"});
+		},
+		setActive: (endpoint, versionId) => {
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+				type: "SET_ACTIVE",
+				payload: axios.get("setup/setactive/"+endpoint+"/"+versionId)
+			});
+		},
+		deleteInactive: (endpoint) => {
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+				type: "DELETE_INACTIVE",
+				payload: axios.get("setup/delete/inactive/"+endpoint)
+			});
+		},
+		deleteEndpoint: (endpoint) => {
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+				type: "DELETE_ENDPOINT",
+				payload: axios.get("setup/delete/endpoint/"+endpoint)
+			});
+		},
+		deleteRuleSet: (endpoint, versionId) => {
+			dispatch({type: "SET_INPROGRESS"});
+			dispatch({
+				type: "DELETE_RULESET",
+				payload: axios.get("setup/delete/ruleset/"+endpoint+"/"+versionId)
+			});
+		},
+		selectDataSet: (dataSetInfo) => {
+			dispatch({type: "SELECT_DATA_SET", payload: dataSetInfo});
+		}
+	}
+};
+
+export default mapDispatchToProps;
--- src/main/js/containers/App.js
+++ src/main/js/containers/App.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import { Route } from 'react-router-dom';
 import ResizeAware from 'react-resize-aware';
 import { Spinner, Notification } from "react-lightning-design-system";
+import mapDispatchToProps from "../actions/appActions"
 import { NavigationBar } from '../components/NavigationBar'
 import { RuleSetSelection } from '../components/RuleSetSelection'
 import { FileUpload } from '../components/FileUpload'
@@ -134,127 +135,6 @@ const mapStateToProps = (state) => {
 		errorMessage: state.appReducer.errorMessage,
 		infoMessage: state.appReducer.infoMessage
   };
-};
-
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onWindowResize: (width, height) => {
-			dispatch({
-				type: "WINDOW_RESIZE",
-				payload: {width, height}
-			});
-		},
-		doClearMessages() {
-			dispatch({type: "CLEAR_MESSAGES"});
-		},
-		openFileUpload: (width, height) => {
-			dispatch({
-				type: "GOTO_UPLOAD",
-				payload: {width, height}
-			});
-		},
-		setSelectedVersion: (endpoint, version) => {
-			dispatch({
-				type: "SET_SELECTED_VERSION",
-				payload: {endpoint,version}
-			});
-		},
-		loadEndpoints: () => {
-			console.log("LOAD ENDPOINTS.....");
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-  			type: "GET_ENDPOINTS",
-  			payload: axios.get("setup/endpoints")
-			});
-		},
-		loadDataSets: () => {
-			console.log("LOAD LISTS.....");
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-  			type: "GET_DATASETS",
-  			payload: axios.get("dataset/all")
-			});
-		},
-		selectEndpoint: (endpoint) => {
-			console.log("LOAD VERSIONS for "+endpoint);
-			dispatch({
-				type: "SET_SELECTED_ENDPOINT",
-				payload: endpoint
-			});
-			//dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-  			type: "GET_VERSIONS",
-  			payload: axios.get("setup/versions/"+endpoint)
-			});
-		},
-		selectVersion: (endpoint, versionId) => {
-			console.log("LOAD RULESET for "+endpoint+"/"+versionId);
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-  			type: "GET_RULESET",
-  			payload: axios.get("setup/source/"+endpoint+"/"+versionId)
-			});
-		},
-		runUnitTests: (endpoint, versionId) => {
-			console.log("RUNNING UNIT TESTS for "+endpoint+"/"+versionId);
-			//dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-  			type: "RUN_UNIT_TESTS",
-  			payload: axios.get("run_unittests/"+endpoint+"/"+versionId)
-			});
-		},
-		setRunNowParameterValue: (name, value) => {
-			dispatch({
-  			type: "SET_RUNNOW_PARAMETER_VALUE",
-  			payload: {parameterName: name, parameterValue: value}
-			});
-		},
-		runNow: (endpoint, versionId, parameters) => {
-			var parameterStr="";
-			Object.keys(parameters).map((parameterName, index) => {
-				parameterStr+= "&"+parameterName+'='+encodeURIComponent(parameters[parameterName]);
-			});
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-				type: "RUN_NOW",
-				payload: axios.get("json/run/"+endpoint+"/"+versionId+"?trace=Y&"+parameterStr)
-			});
-		},
-		runNowClearResult: () => {
-			dispatch({type: "RUN_NOW_CLEAR_RESULT"});
-		},
-		setActive: (endpoint, versionId) => {
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-				type: "SET_ACTIVE",
-				payload: axios.get("setup/setactive/"+endpoint+"/"+versionId)
-			});
-		},
-		deleteInactive: (endpoint) => {
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-				type: "DELETE_INACTIVE",
-				payload: axios.get("setup/delete/inactive/"+endpoint)
-			});
-		},
-		deleteEndpoint: (endpoint) => {
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-				type: "DELETE_ENDPOINT",
-				payload: axios.get("setup/delete/endpoint/"+endpoint)
-			});
-		},
-		deleteRuleSet: (endpoint, versionId) => {
-			dispatch({type: "SET_INPROGRESS"});
-			dispatch({
-				type: "DELETE_RULESET",
-				payload: axios.get("setup/delete/ruleset/"+endpoint+"/"+versionId)
-			});
-		},
-		selectDataSet: (dataSetInfo) => {
-			dispatch({type: "SELECT_DATA_SET", payload: dataSetInfo});
-		}
-	}
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
