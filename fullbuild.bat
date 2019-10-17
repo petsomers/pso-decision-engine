22cd2ea44ef8ed58b4abfc00802f686aff6b694f
BESTORMY <Yentl.Storms@sony.com>
2019-10-17 15:06:50 +0200

    Update build script

--- fullbuild.bat
+++ fullbuild.bat
@@ -1,6 +1,6 @@
 cd frontend/engine-interface
-call npm install
-call npm run build
+call npm install || exit /b
+call npm run build || exit /b
 cd ../..
 call robocopy frontend/engine-interface/build src/main/resources/static/engine-interface /S /E
-call mvn clean package -Dspring.config.location=file:appconfig/pso-decision-engine/ -P release-war
+call mvn clean package -Dspring.config.location=file:appconfig/pso-decision-engine/ -P release-war || exit /b
